11adf46e8c86cce72f7e6c0697371cbe4e8148fe
Muhammad Afaque Khan <mak.khan199@gmail.com>
2021-02-09 13:56:19 +0100

    added controllerAdvice for mvc Exceptions

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -6,6 +6,7 @@ import org.apache.coyote.Response;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
--- src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -5,6 +5,7 @@ import guru.springframework.msscbrewery.web.model.CustomerDto;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.ConstraintViolationException;
@@ -47,16 +48,6 @@ public class CustomerController {
     public void deleteById(@PathVariable("customerId") UUID customerId){
         customerService.deleteById(customerId);
     }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e){
-        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
-        e.getConstraintViolations().forEach(constraintViolation -> {
-            errors.add(constraintViolation.getPropertyPath()+" : "+constraintViolation.getMessage());
-        });
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
-
 
 }
 
